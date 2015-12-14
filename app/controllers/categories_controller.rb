606c4efe0f5c7097e48ae74537a710d75d3e3ec0
Nick Riddell <nickjriddell@gmail.com>
2015-12-14 14:47:13 +0000

    categories set up

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,28 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    Category.create(category_params)
+    redirect_to( categories_path )
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    redirect_to( categories_path )
+  end
+
+  private
+  def category_params
+    params.require(:categoty).permit(:title)
+  end
 end
