8f3dc9d758a834e35e6731da3508e4edb746efb0
Nick Riddell <nickjriddell@gmail.com>
2015-12-18 20:02:19 +0000

    some commented out search function code going to try and make it work.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,10 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    # if params[:search]
+    #   @recipes = Recipe.search(params[:search])
+    # else
+      @recipes = Recipe.all
+    # end
   end
 
   def new
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
   belongs_to :category
+
+  # attr_accessor :title, :ingredients
+
+  # validates :title, presence: true, uniqueness: true
+  # validates :ingredients, presence: true
+
+  # def self.search(query)
+  #   where("title like ?", "%#{query}%")
+  # end
 end
